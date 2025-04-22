af28282c4f57bc4357001291b51b95a6759fda93
Brandon Jones <brandonjones435@u.boisestate.edu>
2025-04-21 19:19:30 -0600

    Work in Progress

--- 4_21_25.py
+++ 4_21_25.py
@@ -1,4 +1,16 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
+# Assigns random number generator to rng
+rng = np.random.default_rng(seed=12345)
+
+# Create 100 random x and y values 
+x = rng.random(100)
+y = rng.random(100)
+
+# equivalent to above using subplots function
+fig, axes = plt.subplots(2,2) 
+axes.scatter(x,y)
+
+axes.plot()
